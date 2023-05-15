d2ce074e6e4b1b632cc4ec6ade6ad569dfc83d2e
TeddyHuang-00 <teddyhuangnan@gmail.com>
2023-05-15 17:34:14 +0800

    fix: :zap: Use timeout for debouncing; adjust search threshold for more precise matches

--- pages/index.vue
+++ pages/index.vue
@@ -10,23 +10,27 @@
             <Input
               placeholder="enter gene name or ENSEMBL ID"
               label="Gene name"
-              v-model="geneSearchTerm"
+              v-model.trim="geneSearchTerm"
+              @input="updateSearchResults"
             >
               <template #prefix>
                 <Icon name="🔎"></Icon>
               </template>
             </Input>
-            <ListGroup class="w-full" v-if="geneSearchResults && !isExactMatch">
-              <ListGroupItem
-                v-for="term in geneSearchResults"
-                @click="geneSearchTerm = term.item.gene_name"
-              >
-                <b>
-                  {{ term.item.gene_name }}
-                </b>
-                - {{ term.item.ENSEMBL }}
-              </ListGroupItem>
-            </ListGroup>
+            <div
+              v-if="geneSearchResults && !isExactMatch"
+              class="pt-2 w-full max-h-60 overflow-scroll"
+            >
+              <ListGroup class="w-full">
+                <ListGroupItem
+                  v-for="term in geneSearchResults"
+                  @click="selectSuggestion(term)"
+                >
+                  {{ term }}
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+            <div v-if="isExactMatch">This will be the detail of a gene</div>
           </div>
         </Tab>
       </Tabs>
@@ -44,22 +48,35 @@ import {
   Input,
 } from "flowbite-vue";
 import Fuse from "fuse.js";
+import GeneDetailType from "~/types/GeneDetailType";
 
 const activeTab = ref("first");
+const totalPageNum = await useLazyFetch("/api/getPageSize");
 const { data: genes } = await useLazyFetch("/api/getGeneList");
 
-const geneSearch = new Fuse(genes.value || [], {
-  keys: ["gene_name", "ENSEMBL"],
-});
+const geneSearch = new Fuse(genes.value || [], { threshold: 0.3 });
 const geneSearchTerm = ref("");
-const geneSearchResults = computed(() => {
-  if (geneSearchTerm.value == "") return [];
-  return geneSearch.search(geneSearchTerm.value);
-});
-const isExactMatch = computed(() => {
-  return (
-    geneSearchResults.value.length != 0 &&
-    geneSearchTerm.value == geneSearchResults.value[0].item.gene_name
-  );
-});
+const geneSearchResults = ref([] as string[]);
+const isExactMatch = ref(false);
+const searchTimeout = ref(0 as number | ReturnType<typeof setTimeout>);
+
+const selectSuggestion = (gene: string) => {
+  geneSearchTerm.value = gene;
+  geneSearchResults.value = [];
+  isExactMatch.value = true;
+};
+const updateSearchResults = () => {
+  clearTimeout(searchTimeout.value);
+  searchTimeout.value = setTimeout(() => {
+    if (geneSearchTerm.value) {
+      geneSearchResults.value = geneSearch
+        .search(geneSearchTerm.value)
+        .map((r) => r.item);
+      isExactMatch.value = geneSearchResults.value[0] === geneSearchTerm.value;
+    } else {
+      geneSearchResults.value = [];
+      isExactMatch.value = false;
+    }
+  }, 500);
+};
 </script>
