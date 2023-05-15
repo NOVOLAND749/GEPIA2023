a30155eb69013bea7f38a47c31a7dc49ecd71316
TeddyHuang-00 <teddyhuangnan@gmail.com>
2023-05-15 13:44:35 +0800

    feat: :sparkles: Use an arrow to control sidebar; remove old button behavior

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "nuxt-icon"],
   runtimeConfig: {
     apiBase: process.env.API_BASE_URL,
   },
--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,43 @@
 <template>
   <div>
-    <div
-      v-if="showSidebar"
-      class="absolute left-0 max-w-sm"
-      @click="showSidebar = false"
-    >
-      <div v-if="abbrsPending"></div>
-      <div v-else-if="!abbrs">
-        <TheCard class="bg-yellow-100 mx-5 my-4">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-yellow-900">
-            Something went wrong, please try again later.
-          </h5>
-        </TheCard>
+    <div class="absolute left-0 max-w-sm mb-20 mt-2 ml-2 flex flex-row">
+      <div v-if="showSidebar">
+        <div v-if="abbrsPending"></div>
+        <div v-else-if="!abbrs">
+          <TheCard class="bg-yellow-100">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-yellow-900">
+              Something went wrong, please try again later.
+            </h5>
+          </TheCard>
+        </div>
+        <div v-else>
+          <ListGroup class="w-fit">
+            <ListGroupItem v-for="ab in abbrs">
+              <div class="w-10 mr-3 text-right font-bold">
+                {{ ab.abbr }}
+              </div>
+              {{ ab.full }}
+            </ListGroupItem>
+          </ListGroup>
+        </div>
       </div>
-      <div v-else>
-        <ListGroup class="w-fit mb-20 mt-2 ml-2">
-          <ListGroupItem v-for="ab in abbrs">
-            <div class="w-10 mr-3 text-right">
-              {{ ab.abbr }}
-            </div>
-            {{ ab.full }}
-          </ListGroupItem>
-        </ListGroup>
+      <div @click="showSidebar = !showSidebar" class="cursor-pointer">
+        <Icon
+          name="material-symbols:chevron-right-rounded"
+          size="2rem"
+          class="ease-out duration-100"
+          :class="{ 'rotate-180': showSidebar }"
+        ></Icon>
       </div>
     </div>
-    <div class="max-w-4xl mx-auto py-4">
-      <Button @click="showSidebar = true">Show disease abbreviation </Button>
-    </div>
+    <div class="max-w-4xl mx-auto py-4">Main content</div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { TheCard, Button, ListGroup, ListGroupItem } from "flowbite-vue";
+import { TheCard, ListGroup, ListGroupItem } from "flowbite-vue";
 const { data: abbrs, pending: abbrsPending } = await useFetch(
   "/api/diseaseAbbr"
 );
-const showSidebar = ref(true);
+const showSidebar = ref(false);
 </script>
