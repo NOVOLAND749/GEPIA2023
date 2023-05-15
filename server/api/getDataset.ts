5004f1c26dbe07b7de5522a55485a18f9bd65d4f
TeddyHuang-00 <teddyhuangnan@gmail.com>
2023-05-15 14:26:31 +0800

    feat: :sparkles: Add api for fetching dataset name list

---
+++ server/api/getDataset.ts
@@ -0,0 +1,6 @@
+import { DatasetType } from "~/types/DatasetType";
+
+export default defineEventHandler(async () => {
+  const { apiBase } = useRuntimeConfig();
+  return await $fetch<DatasetType[]>(apiBase + "/datasets");
+});
---
+++ types/DatasetType.ts
@@ -0,0 +1,8 @@
+export interface DatasetType {
+  id: number;
+  client_name: string;
+  port: number;
+  db_name: string;
+  collection_name: string;
+  description: string;
+}
