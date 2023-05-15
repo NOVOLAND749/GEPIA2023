3819e3cc03e64e95f86a807d4bbf0ab1527a3152
TeddyHuang-00 <teddyhuangnan@gmail.com>
2023-05-15 15:00:49 +0800

    fix: :label: Add type info in fetch

--- server/api/getDataset/[collectionName].ts
+++ server/api/getDataset/[collectionName].ts
@@ -1,5 +1,7 @@
+import DatasetType from "~/types/DatasetType";
+
 export default defineEventHandler(async (event) => {
   const collectionName = event.context.params!.collectionName;
   const { apiBase } = useRuntimeConfig();
-  return await $fetch(apiBase + `/datasets/${collectionName}`);
+  return await $fetch<DatasetType[]>(apiBase + `/datasets/${collectionName}`);
 });
