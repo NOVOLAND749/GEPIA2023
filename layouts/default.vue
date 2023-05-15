2b6e22e8e0a6b4fabfc6437e85fb683608e5fac5
TeddyHuang-00 <teddyhuangnan@gmail.com>
2023-05-15 23:55:08 +0800

    fix: :lipstick: Remove unnecessary arbitary class

--- layouts/default.vue
+++ layouts/default.vue
@@ -34,7 +34,7 @@
     </div>
 
     <footer
-      class="fixed bottom-0 z-20 border-t-[1px] w-full bg-transparent backdrop-blur-sm shadow-3xl"
+      class="fixed bottom-0 z-20 border-t w-full bg-transparent backdrop-blur-sm shadow-3xl"
     >
       <div class="w-full mx-auto p-6 text-center">
         &copy; 2023 XXXX. All rights reserved.
