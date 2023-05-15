dddb0a3a37bb868a3c63bb17ffc41d01ab2b0844
TeddyHuang-00 <teddyhuangnan@gmail.com>
2023-05-15 15:49:59 +0800

    feat: :sparkles: Add first tab and search gene functionality

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,62 @@
       <h1 class="py-8 text-3xl text-center text-gray-600 font-bold mx-auto">
         GEPIA 2023
       </h1>
-      <div>Main content</div>
+      <Tabs v-model="activeTab" variant="underline">
+        <Tab name="first" title="View genes">
+          <div class="py-2">
+            <Input
+              placeholder="enter gene name or ENSEMBL ID"
+              label="Gene name"
+              v-model="geneSearchTerm"
+            >
+              <template #prefix>
+                <Icon name="🔎"></Icon>
+              </template>
+            </Input>
+            <ListGroup class="w-full" v-if="geneSearchResults && !isExactMatch">
+              <ListGroupItem
+                v-for="term in geneSearchResults"
+                @click="geneSearchTerm = term.item.gene_name"
+              >
+                <b>
+                  {{ term.item.gene_name }}
+                </b>
+                - {{ term.item.ENSEMBL }}
+              </ListGroupItem>
+            </ListGroup>
+          </div>
+        </Tab>
+      </Tabs>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  Dropdown,
+  ListGroup,
+  ListGroupItem,
+  Tab,
+  Tabs,
+  Input,
+} from "flowbite-vue";
+import Fuse from "fuse.js";
+
+const activeTab = ref("first");
+const { data: genes } = await useLazyFetch("/api/getGeneList");
+
+const geneSearch = new Fuse(genes.value || [], {
+  keys: ["gene_name", "ENSEMBL"],
+});
+const geneSearchTerm = ref("");
+const geneSearchResults = computed(() => {
+  if (geneSearchTerm.value == "") return [];
+  return geneSearch.search(geneSearchTerm.value);
+});
+const isExactMatch = computed(() => {
+  return (
+    geneSearchResults.value.length != 0 &&
+    geneSearchTerm.value == geneSearchResults.value[0].item.gene_name
+  );
+});
+</script>
