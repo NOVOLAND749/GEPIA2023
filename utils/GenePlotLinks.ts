05500d03d0a9470eff71ada8dc4fe79549840e9b
TeddyHuang-00 <teddyhuangnan@gmail.com>
2023-05-16 14:37:43 +0800

    feat: :sparkles: Add api and functionality for fetching bar plot of a gene

--- utils/GenePlotLinks.ts
+++ utils/GenePlotLinks.ts
@@ -4,6 +4,10 @@ const getStripPlotLink = (gene: GeneDetailType) => {
   return `/api/getStripPlot/${gene.gene_name}`;
 };
 
+const getBarPlotLink = (gene: GeneDetailType) => {
+  return `/api/getBarPlot/${gene.gene_name}`;
+};
+
 export function getGenePlotLinks(gene: GeneDetailType) {
-  return { StripPlot: getStripPlotLink(gene) };
+  return { StripPlot: getStripPlotLink(gene), BarPlot: getBarPlotLink(gene) };
 }
