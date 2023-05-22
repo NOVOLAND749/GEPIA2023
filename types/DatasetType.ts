1e5feeca95a708ee4ba3e1264aaa4d36f2651fdc
TeddyHuang-00 <teddyhuangnan@gmail.com>
2023-05-22 11:49:03 +0800

    fix: :alien: Change dataset fetching related API to match with backend

--- types/DatasetType.ts
+++ types/DatasetType.ts
@@ -1,8 +1,8 @@
 export default interface DatasetType {
   id: number;
-  client_name: string;
-  port: number;
   db_name: string;
-  collection_name: string;
+  tumor_sample_number: number;
+  normal_sample_number: number;
+  has_normal: boolean;
   description: string;
 }
