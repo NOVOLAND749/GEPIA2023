051c934c24400d7a1c2d9656ed37c67a5afbd2fd
TeddyHuang-00 <teddyhuangnan@gmail.com>
2023-05-22 19:50:36 +0800

    feat: :sparkles: Add DiffExp tab and basic input; refactor tabs into individual SFC

---
+++ components/DiffExp.vue
@@ -0,0 +1,137 @@
+<template>
+  <div>
+    <div class="flex flex-row gap-2">
+      <div class="flex-grow min-w-fit my-auto">
+        Dataset:
+        <Dropdown placement="bottom">
+          <template #trigger="{ toggle }">
+            <Button @click="toggle">
+              {{ datasetSelection ? datasetSelection.db_name : "Choose one" }}
+            </Button>
+          </template>
+          <ListGroup class="max-h-60 overflow-y-scroll">
+            <ListGroupItem
+              v-for="ds in datasets"
+              :disabled="!ds.has_normal"
+              @click="
+                datasetSelection = ds.has_normal ? ds : datasetSelection;
+                catchDatasetError(ds);
+              "
+            >
+              {{ ds.db_name }}
+            </ListGroupItem>
+          </ListGroup>
+        </Dropdown>
+      </div>
+      <div class="w-1/3 min-w-fit">
+        <Input
+          placeholder="1.00"
+          label="|log₂ Fold-Change| Cutoff"
+          v-model.number="log2FC"
+        >
+          <template #prefix>
+            <Icon name="material-symbols:123" size="24"></Icon>
+          </template>
+        </Input>
+      </div>
+      <div class="w-1/3 min-w-fit">
+        <Input
+          placeholder="1.00"
+          label="Q-value Cutoff"
+          v-model.number="qValue"
+        >
+          <template #prefix>
+            <Icon name="material-symbols:123" size="24"></Icon>
+          </template>
+        </Input>
+      </div>
+    </div>
+
+    <Teleport to="body">
+      <div class="absolute top-20 right-0 flex flex-col gap-2">
+        <Toast
+          v-for="toast in toastList"
+          :type="toast.type"
+          class="border shadow-md"
+        >
+          {{ toast.message }}
+        </Toast>
+      </div>
+    </Teleport>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {
+  Spinner,
+  ListGroup,
+  ListGroupItem,
+  Input,
+  Button,
+  Dropdown,
+  Toast,
+} from "flowbite-vue";
+import { ToastType } from "flowbite-vue/dist_types/components/Toast/types";
+import DatasetType from "~/types/DatasetType";
+
+const toastList = ref([] as { type: ToastType; message: string }[]);
+const toastProvider = (
+  toast: { type: ToastType; message: string },
+  timeout: number = 3000
+) => {
+  toastList.value.push(toast);
+  setTimeout(() => {
+    toastList.value.shift();
+  }, timeout);
+};
+
+const datasetStore = useDatasetStore();
+const datasets = ref(datasetStore.datasetList);
+const datasetSelection = ref(null as DatasetType | null);
+
+const catchDatasetError = (dataset: DatasetType) => {
+  if (!dataset.has_normal) {
+    toastProvider({
+      type: "warning",
+      message: `You cannot select the ${dataset.db_name} dataset as it does not have normal samples.`,
+    });
+  }
+};
+
+const log2FC = ref("1.0");
+const qValue = ref("0.01");
+
+const log2FCNumber = computed(() => {
+  return parseFloat(log2FC.value);
+});
+const qValueNumber = computed(() => {
+  return parseFloat(qValue.value);
+});
+
+watch(log2FCNumber, (log2FCNumber) => {
+  if (log2FCNumber <= 0.0) {
+    toastProvider({
+      type: "danger",
+      message: `The log₂ Fold-Change cutoff value ${log2FCNumber} must be positive.`,
+    });
+  }
+});
+
+watch(qValueNumber, (qValueNumber) => {
+  if (qValueNumber <= 0.0 || qValueNumber >= 1.0) {
+    toastProvider({
+      type: "danger",
+      message: `The Q-value cutoff value ${qValueNumber} must be between 0 and 1.`,
+    });
+  }
+});
+
+onBeforeMount(async () => {
+  if (!datasets.value.length) {
+    if (!datasetStore.datasetList.length) {
+      await datasetStore.load();
+    }
+    datasets.value = datasetStore.datasetList;
+  }
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -9,40 +9,10 @@
       </h1>
       <Tabs v-model="activeTab" variant="underline">
         <Tab name="first" title="GeneView">
-          <div class="py-2">
-            <Input
-              placeholder="Enter a gene name, e.g. ERBB2"
-              label="Search for a gene"
-              v-model.trim="geneSearchTerm"
-              @input="updateSearchResults"
-            >
-              <template #prefix>
-                <Icon
-                  name="material-symbols:search"
-                  class="transform -scale-x-100"
-                ></Icon>
-              </template>
-            </Input>
-            <div
-              v-if="geneSearchResults && !isExactMatch"
-              class="pt-2 w-full max-h-60 overflow-scroll"
-            >
-              <ListGroup class="w-full">
-                <ListGroupItem
-                  v-for="term in geneSearchResults"
-                  @click="selectSuggestion(term)"
-                >
-                  {{ term }}
-                </ListGroupItem>
-              </ListGroup>
-            </div>
-            <div v-if="geneDetail" class="pt-2 px-2">
-              <GeneDetail :gene="geneDetail"></GeneDetail>
-            </div>
-            <div v-else-if="isExactMatch" class="w-full mx-auto pt-3">
-              <Spinner size="12" color="white" />
-            </div>
-          </div>
+          <GeneView class="py-2"></GeneView>
+        </Tab>
+        <Tab name="second" title="DiffExp">
+          <DiffExp class="py-2"></DiffExp>
         </Tab>
       </Tabs>
     </div>
@@ -50,82 +20,7 @@
 </template>
 
 <script setup lang="ts">
-import {
-  Spinner,
-  ListGroup,
-  ListGroupItem,
-  Tab,
-  Tabs,
-  Input,
-} from "flowbite-vue";
-import Fuse from "fuse.js";
-import GeneDetailType from "~/types/GeneDetailType";
-
-const route = useRoute();
-const router = useRouter();
+import { Tab, Tabs } from "flowbite-vue";
 
 const activeTab = ref("first");
-const geneStore = useGeneStore();
-const genes = ref(geneStore.geneList);
-
-const geneSearch = new Fuse(genes.value || [], { threshold: 0.3 });
-const geneSearchTerm = ref(
-  route.query.gene ? (route.query.gene as string) : ""
-);
-const geneSearchResults = ref([] as string[]);
-const isExactMatch = ref(route.query.gene ? true : false);
-const searchTimeout = ref(0 as number | ReturnType<typeof setTimeout>);
-
-const selectSuggestion = (gene: string) => {
-  geneSearchTerm.value = gene;
-  geneSearchResults.value = [];
-  isExactMatch.value = true;
-  router.push({ query: { gene: gene } });
-};
-const updateSearchResults = () => {
-  clearTimeout(searchTimeout.value);
-  searchTimeout.value = setTimeout(() => {
-    if (geneSearchTerm.value) {
-      geneSearchResults.value = geneSearch
-        .search(geneSearchTerm.value)
-        .map((r) => r.item);
-      isExactMatch.value = geneSearchResults.value[0] === geneSearchTerm.value;
-    } else {
-      geneSearchResults.value = [];
-      isExactMatch.value = false;
-    }
-  }, 500);
-};
-
-const geneDetail = ref(null as GeneDetailType | null);
-
-watch(isExactMatch, async (isExactMatch) => {
-  if (isExactMatch) {
-    geneDetail.value = null;
-    if (geneSearchTerm.value) {
-      geneDetail.value = await $fetch(
-        `/api/getGeneDetail/${geneSearchTerm.value}`
-      );
-    }
-  }
-});
-
-onBeforeMount(async () => {
-  if (!genes.value.length) {
-    if (!geneStore.geneList.length) {
-      await geneStore.loadFromLocalStorage();
-    }
-    genes.value = geneStore.geneList;
-    geneSearch.setCollection(genes.value);
-  }
-
-  if (isExactMatch.value) {
-    geneDetail.value = null;
-    if (geneSearchTerm.value) {
-      geneDetail.value = await $fetch(
-        `/api/getGeneDetail/${geneSearchTerm.value}`
-      );
-    }
-  }
-});
 </script>
